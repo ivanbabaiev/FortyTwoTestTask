22f3c2f7bf7db7146ab0fa74d1bb10d8bcfa89be
ivanbabaiev <ivanbabaiev@gmail.com>
2016-05-31 22:36:27 +0300

    Added test middleware and view Ver.1

--- apps/hello/models.py
+++ apps/hello/models.py
@@ -19,3 +19,17 @@ class Person(models.Model):
     def __unicode__(self):
         full_name = "%s %s" % (self.name, self.surname)
         return full_name
+
+
+class Request(models.Model):
+    """
+    Create Requests model
+    """
+    date_time = models.DateTimeField(auto_now_add=True)
+    path = models.CharField(max_length=255)
+    server_protocol = models.CharField(max_length=50)
+    request_method = models.CharField(max_length=50)
+    status_code = models.IntegerField(max_length=10)
+
+    class Meta:
+        ordering = ['date_time']
