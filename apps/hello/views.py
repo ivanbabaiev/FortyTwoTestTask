57313ba7c80938fef54a5f14dbc076328e4584d7
ivanbabaiev <ivanbabaiev@gmail.com>
2016-06-02 12:35:26 +0300

    Added test for middleware - test fail

--- apps/hello/views.py
+++ apps/hello/views.py
@@ -3,7 +3,6 @@
 from django.shortcuts import render_to_response
 
 from apps.hello.models import Person
-from apps.hello.models import Request
 
 
 def person_list_view(request):
@@ -12,11 +11,3 @@ def person_list_view(request):
     """
     person = Person.objects.first()
     return render_to_response('hello/index.html', {'person': person})
-
-
-def requests_list_view(request):
-    """
-    View for request page
-    """
-    req_list = Request.objects.order_by('date_time')[:10]
-    return render_to_response('hello/requests.html', {'requests': req_list})
