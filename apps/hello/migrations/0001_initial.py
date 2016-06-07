19d047c6061e5db81e85a986dd0f5a230a845e12
ivanbabaiev <ivanbabaiev@gmail.com>
2016-06-07 13:09:47 +0300

    ticket:3 refactoring migrations Ver1.4

--- apps/hello/migrations/0001_initial.py
+++ apps/hello/migrations/0001_initial.py
@@ -9,14 +9,63 @@ class Migration(SchemaMigration):
 
     def forwards(self, orm):
         # Adding model 'Person'
-        pass
+        db.create_table(u'hello_person', (
+            (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('name', self.gf('django.db.models.fields.CharField')(max_length=50)),
+            ('surname', self.gf('django.db.models.fields.CharField')(max_length=50)),
+            ('date_of_birth', self.gf('django.db.models.fields.DateField')()),
+            ('bio', self.gf('django.db.models.fields.TextField')()),
+            ('email', self.gf('django.db.models.fields.CharField')(max_length=50)),
+            ('jabber', self.gf('django.db.models.fields.CharField')(max_length=50)),
+            ('skype', self.gf('django.db.models.fields.CharField')(max_length=50)),
+            ('other_contacts', self.gf('django.db.models.fields.TextField')()),
+        ))
+        db.send_create_signal(u'hello', ['Person'])
+
+        # Adding model 'Request'
+        db.create_table(u'hello_request', (
+            (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('date_time', self.gf('django.db.models.fields.DateTimeField')(auto_now_add=True, blank=True)),
+            ('path', self.gf('django.db.models.fields.CharField')(max_length=255)),
+            ('server_protocol', self.gf('django.db.models.fields.CharField')(max_length=50)),
+            ('request_method', self.gf('django.db.models.fields.CharField')(max_length=50)),
+            ('status_code', self.gf('django.db.models.fields.IntegerField')(max_length=10)),
+            ('viewed', self.gf('django.db.models.fields.BooleanField')(default=False)),
+        ))
+        db.send_create_signal(u'hello', ['Request'])
+
 
     def backwards(self, orm):
         # Deleting model 'Person'
-        pass
+        db.delete_table(u'hello_person')
+
+        # Deleting model 'Request'
+        db.delete_table(u'hello_request')
+
 
     models = {
-
+        u'hello.person': {
+            'Meta': {'object_name': 'Person'},
+            'bio': ('django.db.models.fields.TextField', [], {}),
+            'date_of_birth': ('django.db.models.fields.DateField', [], {}),
+            'email': ('django.db.models.fields.CharField', [], {'max_length': '50'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'jabber': ('django.db.models.fields.CharField', [], {'max_length': '50'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '50'}),
+            'other_contacts': ('django.db.models.fields.TextField', [], {}),
+            'skype': ('django.db.models.fields.CharField', [], {'max_length': '50'}),
+            'surname': ('django.db.models.fields.CharField', [], {'max_length': '50'})
+        },
+        u'hello.request': {
+            'Meta': {'ordering': "['date_time']", 'object_name': 'Request'},
+            'date_time': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'path': ('django.db.models.fields.CharField', [], {'max_length': '255'}),
+            'request_method': ('django.db.models.fields.CharField', [], {'max_length': '50'}),
+            'server_protocol': ('django.db.models.fields.CharField', [], {'max_length': '50'}),
+            'status_code': ('django.db.models.fields.IntegerField', [], {'max_length': '10'}),
+            'viewed': ('django.db.models.fields.BooleanField', [], {'default': 'False'})
+        }
     }
 
     complete_apps = ['hello']
